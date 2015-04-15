f3552bdde219ef885176efa3eaf509c5312f9748
Kyle Bulloch & Tom Mertz <student@epicodus.com>
2015-04-15 15:23:28 -0700

    Update specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,28 @@
 var Patient = {
   getRisky: function() {
 
-    if(this.diet === 1) {
-      return ["Obesity"];
+    var risks = [];
+
+    if (this.diet === 1 && this.exercise === 1) {
+      risks.push("Obesity");
+      risks.push("Heart Disease");
+    }
+    else if (this.diet === 1) {
+      risks.push("Diabetes");
     }
     if (this.blood_pressure === 1) {
-      return ["Heart Disease"];
+      risks.push("Heart Disease");
     }
     if (this.cancer_history === 1) {
-      return ["Cancer"];
+      risks.push("Cancer");
     }
-    return ["Healthy"];
+    if (this.smokes === 1) {
+      risks.push("Heart Disease", "Cancer");
+    }
+    if (risks.length === 0) {
+      risks.push("Healthy");
+    }
+    return risks.sort();
   }
 };
 
--- spec/specs.js
+++ spec/specs.js
@@ -11,17 +11,17 @@ describe('Patient',function() {
       expect(newPatient.getRisky()).to.eql(['Healthy']);
     });
 
-    it("will return a risk of 'Obesity' for a normal healthy person", function() {
+    it("will return a risk of 'Heart Disease', 'Obesity' for a patient with bad diet and exercise", function() {
       var newPatient = Object.create(Patient);
       newPatient.diet = 1;
-      newPatient.exercise = 0;
+      newPatient.exercise = 1;
       newPatient.smokes = 0;
       newPatient.blood_pressure = 0;
       newPatient.cancer_history = 0;
-      expect(newPatient.getRisky()).to.eql(['Obesity']);
+      expect(newPatient.getRisky()).to.eql(['Heart Disease', 'Obesity']);
     });
 
-    it("will return a risk of 'Heart Disease' for a normal healthy person", function() {
+    it("will return a risk of 'Heart Disease' for a patient with high blood pressure", function() {
       var newPatient = Object.create(Patient);
       newPatient.diet = 0;
       newPatient.exercise = 0;
@@ -31,7 +31,7 @@ describe('Patient',function() {
       expect(newPatient.getRisky()).to.eql(['Heart Disease']);
     });
 
-    it("will return a risk of 'Cancer' for a normal healthy person", function() {
+    it("will return a risk of 'Cancer'", function() {
       var newPatient = Object.create(Patient);
       newPatient.diet = 0;
       newPatient.exercise = 0;
@@ -39,6 +39,36 @@ describe('Patient',function() {
       newPatient.blood_pressure = 0;
       newPatient.cancer_history = 1;
       expect(newPatient.getRisky()).to.eql(['Cancer']);
+    });
+
+    it("will return a risk of 'Cancer', 'Heart Disease'", function() {
+      var newPatient = Object.create(Patient);
+      newPatient.diet = 0;
+      newPatient.exercise = 1;
+      newPatient.smokes = 1;
+      newPatient.blood_pressure = 0;
+      newPatient.cancer_history = 0;
+      expect(newPatient.getRisky()).to.eql(['Cancer', 'Heart Disease']);
+    });
+
+    it("will return a risk of 'Diabetes'", function() {
+      var newPatient = Object.create(Patient);
+      newPatient.diet = 1;
+      newPatient.exercise = 0;
+      newPatient.smokes = 0;
+      newPatient.blood_pressure = 0;
+      newPatient.cancer_history = 0;
+      expect(newPatient.getRisky()).to.eql(['Diabetes']);
+    });
+
+    it("will return a risk of 'Cancer', 'Heart Disease', 'Obesity'", function() {
+      var newPatient = Object.create(Patient);
+      newPatient.diet = 1;
+      newPatient.exercise = 1;
+      newPatient.smokes = 0;
+      newPatient.blood_pressure = 0;
+      newPatient.cancer_history = 1;
+      expect(newPatient.getRisky()).to.eql(['Cancer', 'Heart Disease', 'Obesity']);
     });
   });
 
